645dc1cb8975d1242931ed65188b6aaf6d6f05e6
AntonSayetharayeu <antonsayet@gmail.com>
2023-12-16 22:40:28 +0100

    [New Feature] Registration functionality added

--- SecurityManager_Fun/Logic/AccountService.cs
+++ SecurityManager_Fun/Logic/AccountService.cs
@@ -23,7 +23,35 @@ namespace SecurityManager_Fun.Logic
 
             string[] passwordParts = employee.Password.Split(":");
 
-            return (VerifyPassword(password, passwordParts[0], Encoding.UTF8.GetBytes(passwordParts[1])) ? employee : null);
+            return employee.Password == password ? employee : null;
+            //return (VerifyPassword(password, passwordParts[0], Encoding.UTF8.GetBytes(passwordParts[1])) ? employee : null);
+        }
+
+        public static void RegisterNewEmployee(string firstName, string lastName, string phoneNumber, int RoleID) { 
+            using (var context = new AppDBContex())
+            {
+                byte[] salt;
+                //TODO: Talk about default values for new Employee
+                string login = string.Format("{0}.{1}", firstName.ToLower(), lastName.ToLower());
+                string defaultPassword = string.Format("{0}:{1}",   
+                                    HashPassword(login + "_SM", out salt),
+                                    salt.ToString());
+                decimal defaultGrossRate = 4000m;
+
+                context.Add(
+                    new Employee
+                    {
+                        Name = firstName,
+                        Surname = lastName,
+                        Phone = phoneNumber,
+                        RoleID = RoleID,
+                        Login = login,
+                        Email = string.Format("{0}.{1}@gmail.com", firstName.ToLower(), lastName.ToLower()), //TODO: talk about email domain
+                        Password = defaultPassword,
+                        GrossRate = defaultGrossRate
+                    });
+                context.SaveChanges();
+            }
         }
 
         //salt with out here is used to generate salt with hash and then write the whole password(with pattern: hash:salt) to the DB
