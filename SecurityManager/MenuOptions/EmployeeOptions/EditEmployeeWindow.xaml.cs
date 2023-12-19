42b2adb3f07093b227689e1a2f680c3251e56235
tekla22 <mactek01@gmail.com>
2023-12-19 14:32:58 +0100

    [New feature] Added EditEmployeeWindow for editing employees.

---
+++ SecurityManager/MenuOptions/EmployeeOptions/EditEmployeeWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SecurityManager_GUI.MenuOptions.EmployeeOptions
+{
+    /// <summary>
+    /// Interaction logic for EditEmployeeWindow.xaml
+    /// </summary>
+    public partial class EditEmployeeWindow : Window
+    {
+        public EditEmployeeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
