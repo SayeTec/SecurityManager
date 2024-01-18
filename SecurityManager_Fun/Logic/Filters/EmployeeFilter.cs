fc943ae4195d365bc7e708c21877ae53d135555b
AntonSayetharayeu <antonsayet@gmail.com>
2024-01-18 23:47:40 +0100

    [Code Feature] Filter logic for Employee created

---
+++ SecurityManager_Fun/Logic/Filters/EmployeeFilter.cs
@@ -0,0 +1,19 @@
+﻿using SecurityManager_Fun.Data.Repositories;
+using SecurityManager_Fun.Model;
+
+namespace SecurityManager_Fun.Logic.Filters
+{
+    public class EmployeeFilter
+    {
+        public static List<Employee> FilterEmployees(string name, string surname, Role role, Department department)
+        {
+            return EmployeeRepository.GetAllEmployees()
+                .Where(emp => 
+                (string.IsNullOrEmpty(name) || emp.Name.Contains(name)) &&
+                (string.IsNullOrEmpty(surname) || emp.Surname.Contains(surname)) &&
+                (department == null || emp.Department.Equals(department)) &&
+                (role == null || emp.Role.Equals(role)))
+                .ToList();
+        }
+    }
+}
